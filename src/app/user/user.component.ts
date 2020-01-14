780974f77f114a447030d0ff9f33af51500bcc07
AnaTopic <atopic.ie@gmail.com>
2020-01-14 11:39:49 +0100

    reactive forms validation

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,38 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { User } from '../user';
-
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms'
+import { emailValidator } from './email-validator'
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
+
+
 export class UserComponent implements OnInit {
 
-  username = new FormControl('');
-  email = new FormControl('');
-  password = new FormControl ('');
-  streetAddress = new FormControl ('');
-  postCode = new FormControl ('');
-  city = new FormControl ('');
-  user = new User();
-
-  constructor() { }
+  userForm:FormGroup;
+  
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.userForm = this.fb.group({
+    
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, emailValidator]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      
+      streetAddress: ['', [Validators.required,]],
+      postCode: ['', [Validators.required,]],
+      city: ['', [Validators.required,]],
+  
+    });
  
   }
 
-  submitForm(){
-    this.user.username = this.username.value;
-    this.user.email =this.email.value;
-    this.user.password =this.password.value;
-    this.user.streetAddress =this.streetAddress.value;
-    this.user.postCode =this.postCode.value;
-    this.user.city =this.city.value;
 
-    console.log(this.username.value)
+  get username() {
+    return this.userForm.get('username');
+  }
+
+  get email() {
+    return this.userForm.get('email');
+  }
+
+  get password() {
+    return this.userForm.get('password');
+  }
+
+  get streetAddress() {
+    return this.userForm.get('streetAddress');
+  }
+
+  get postCode() {
+    return this.userForm.get('postCode');
+  }
+
+  get city() {
+    return this.userForm.get('city');
+  }
+
+  submitForm(){
+
+     console.log(this.userForm.value)
+      
   }
 
   
